0e8b5440cc9c5889fc63c69131cb2a1ca4ee3313
andrewbinkard <andrewbinkard@gmail.com>
2020-07-24 00:56:18 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+var count = document.getElementById("counter")
+var picture = document.getElementById("pic")
+
+setTimeout((function(){
+    count.innerHTML -= 7;
+}), 1000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 2000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 3000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 4000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 5000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 6000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 7000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 8000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 9000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 10000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 11000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 12000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 13000);
+
+setTimeout((function(){
+  count.innerHTML -= 7;
+}), 14000);
+
+setTimeout((function(){
+  picture.innerHTML = '<img src="images/tremendous.jpg">'
+}), 15000)
